7d7417c9cf5e40335bf4cf0608a23002ff1c7c4f
ljopleda <ljopleda@gmail.com>
2016-10-11 15:40:23 +0800

    Adding footer and featured gym

--- footer.php
+++ footer.php
@@ -1,4 +1,66 @@
+		<?php
+			get_template_part( 'partials/content', 'vip-subscriber' );
+		?>
+		<!-- FOOTER -->
+		<footer id="footer">
+			<div class="container padding-20 margin-bottom-10">
+				<div class="text-center">
+					<a href="<?=site_url()?>" class="text-white underline">
+						<img src="<?=get_stylesheet_directory_uri();?>/assets/img/mmc-logo-light.png" class="width-150">
+					</a>
+					<!-- Social Icons -->
+					<div class="text-center margin-top-0">
+						<a href="https://www.facebook.com/marketmasterclasscom-1657855544470731/" target="_blank" class="social-icon social-icon-round social-icon-border social-facebook noborder" data-toggle="tooltip" data-placement="top" title="Facebook">
 
+							<i class="icon-facebook"></i>
+							<i class="icon-facebook"></i>
+						</a>
+
+						<a href="https://twitter.com/MyMarketMaster" target="_blank" class="social-icon social-icon-round social-icon-border social-twitter noborder" data-toggle="tooltip" data-placement="top" title="Twitter">
+							<i class="icon-twitter"></i>
+							<i class="icon-twitter"></i>
+						</a>
+
+						<a href="https://pinterest.com/MyMarketMaster" class="social-icon social-icon-round social-icon-border social-gplus noborder" data-toggle="tooltip" data-placement="top" title="Pinterest">
+							<i class="fa fa-pinterest "></i>
+							<i class="fa fa-pinterest "></i>
+						</a>
+
+						<a href="https://instagram.com/MyMarketMaster" class="social-icon social-icon-round social-icon-border social-instagram noborder" data-toggle="tooltip" data-placement="top" title="Instagram">
+							<i class="icon-instagram"></i>
+							<i class="icon-instagram"></i>
+						</a>
+
+						<a href="https://www.youtube.com/channel/UCI4UNi7DBZMHRYoaszF_CdA" target="_blank" class="social-icon social-icon-round social-icon-border social-youtube noborder" data-toggle="tooltip" data-placement="top" title="Youtube">
+							<i class="icon-youtube"></i>
+							<i class="icon-youtube"></i>
+						</a>
+					</div>
+					<!-- /Social Icons -->
+					<p>
+						Cage Gladiators is part of the <a href="http://www.divestmedia.com" style="color: #0072bc;font-weight: bold;">Divest Media Network</a>
+					</p>
+				</div>
+				<hr/>
+				<div class="row ">
+					<div class="col-sm-6">
+						&copy; 2016 Cage Gladiators. All rights reserved
+					</div>
+					<div class="col-sm-6 text-right">
+						<?php
+						if(!empty(wp_get_nav_menu_items('Footer Navigation'))){
+							$footer_menu = [];
+							foreach (wp_get_nav_menu_items('Footer Navigation') as $f) {
+								array_push($footer_menu , '<a href="'.$f->url.'">'.$f->title.'</a>');
+							}
+						}
+						print_r(implode(' | ', $footer_menu));
+						?>
+					</div>
+				</div>
+			</div>
+		</footer>
+		<!-- /FOOTER -->
 	</div>
 	<!-- /wrapper -->
 	
--- partials/content-featured-gym.php
+++ partials/content-featured-gym.php
@@ -1,1 +1,9 @@
-featured-gym
+<div id="container-featured-gym" class="row">
+	<span class="overlay dark-5"></span>
+	<div class="col-md-4">
+		<div class="uppercase section-title text-white text-center weight-700 size-20 margin-bottom-10">Featured Training GYM</div>
+		<div class="uppercase gym-name weight-700 size-17">ARENA FITNESS MMA</div>
+		<p class="text-white margin-bottom-20">Fabulas definitiones ei pri per recteque hendrerit scriptorem in errem scribentur mel fastidii propriae philosophia cu mea. Utinam ipsum everti necessitatibus at fuisset splendide.</p>
+		<div><button type="button" class="btn btn-default btn-sm uppercase">View Gym Info</button></div>
+	</div>
+</div>
