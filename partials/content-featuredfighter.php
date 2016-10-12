3c842d48326149e7eeb9690c6ca7aa428d41918b
ljopleda <ljopleda@gmail.com>
2016-10-12 16:59:27 +0800

    Main featured fighter added

--- partials/content-featuredfighter.php
+++ partials/content-featuredfighter.php
@@ -1,19 +1,18 @@
 <section id="featured-fighter">
 	<div class="container padding-bottom-20">
 		<div class="row">
-			<div class="col-sm-3 text-right fighter-details">
+			<div class="col-sm-3 text-right fighter-details margin-bottom-20">
 				<p class="text-white size-13">Fabulas definitiones ei pri per recteque hendrerit scriptorem in errem scribentur mel fastidii propriae philosophia cu mea. Utinam ipsum everti necessitatibus at fuisset splendide.</p>
 				<button class="btn btn-danger btn-sm uppercase">Read More</button>
-			</div>
-			<div class="col-sm-9 fighter-details">
+			</div><div class="col-sm-9 fighter-details">
 				<div>
-
 				</div>
 				<div class="fighter-name">
+					<div><img src="/wp-content/themes/cagegladiators/assets/img/road-to-mma-logo-1.png" alt="" style="width: 170px;height: auto;"></div>
 					<h1 class="text-white uppercase size-80" style="margin-bottom: 0;">JOSE</h1>
 					<h1 class="uppercase" style="color:#e60f0f;margin-top: -20px;font-size:120px;">Cuervo</h1>
 				</div>
-				<img src="/wp-content/themes/cagegladiators/assets/img/road-to-mma-fighter-1.png" alt="">
+				<img class="fighter-image" src="/wp-content/themes/cagegladiators/assets/img/road-to-mma-fighter-1.png" alt="">
 			</div>
 		</div>
 	</div>
--- partials/content-main-featuredfighter.php
+++ partials/content-main-featuredfighter.php
@@ -1,1 +1,16 @@
-main-featuredfighter
+
+<section id="main-featured-fighter">
+	<div class="ff-bg-red"></div>
+	<div class="ff-bg-gray">
+		<div class="container padding-bottom-20">
+			<div class="row">
+				<div class="col-sm-3 text-right fighter-details margin-bottom-20">
+					<p class="text-white size-13">Fabulas definitiones ei pri per recteque hendrerit scriptorem in errem scribentur mel fastidii propriae philosophia cu mea. Utinam ipsum everti necessitatibus at fuisset splendide.</p>
+					<button class="btn btn-danger btn-sm uppercase">View Profile</button>
+				</div><div class="col-sm-9 fighter-details">
+					<img class="fighter-image" src="/wp-content/themes/cagegladiators/assets/img/main-featured-fighter-1.png" alt="">
+				</div>
+			</div>
+		</div>
+	</div>
+</section>
