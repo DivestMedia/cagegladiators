4983ef1cb776ee13fc49ce7be169f69f4d551d08
Ralph John Galindo <ralphjesy@gmail.com>
2016-10-13 11:39:40 +0800

    Adding Featured Fighters on Home Page

--- partials/content-side-featuredfighter.php
+++ partials/content-side-featuredfighter.php
@@ -1,18 +1,42 @@
-<div class="content-side-featuredfighter col-md-12 col-sm-6">
-	<div>
-		<div class="uppercase section-title text-white text-center weight-700 size-12">Featured Fighter</div>
-		<h3 class="side-featuredfighter-title text-white uppercase size-17">UFC: Ronda Rousey</h3>
-	</div>
-	<figure>
-		<img class="img-responsive" src="/wp-content/themes/cagegladiators/assets/img/featured-fighter-side-1.png" alt="" />
-	</figure>
-</div>
-<div class="content-side-featuredfighter col-md-12 col-sm-6">
-	<div>
-		<div class="uppercase section-title text-white text-center weight-700 size-12">Featured Fighter</div>
-		<h3 class="side-featuredfighter-title text-white uppercase size-17">UFC: Ronda Rousey</h3>
-	</div>
-	<figure>
-		<img class="img-responsive" src="/wp-content/themes/cagegladiators/assets/img/featured-fighter-side-1.png" alt="" />
-	</figure>
-</div>
+<?php
+$featuredFighter = get_posts([
+	'posts_per_page' => 2,
+	'paged' => 1,
+	'post_type' => [
+		'fighter'
+	],
+	'meta_key' => '_is_featured',
+	'orderby' => '_is_featured',
+	'order' => 'DESC',
+	'meta_query' => [
+		[
+			'key' => '_is_featured',
+			'compare' => 'LIKE',
+		]
+	],
+	'post_status'      => 'publish',
+	]) ?: [];
+	?>
+	<?php if(count($featuredFighter)): ?>
+		<?php foreach ($featuredFighter as $key => $fighter):
+			$imagesrc = '';
+			$sideview = get_post_meta('_uf_image_right') ?: false;
+
+			if($sideview){
+				$imagesrc = wp_get_attachment_image_src($sideview,'full');
+			}
+			if(empty($imagesrc)){
+				$imagesrc = wp_get_attachment_image_src(get_post_thumbnail_id(),'full');
+			}
+			?>
+			<div class="content-side-featuredfighter col-md-12 col-sm-6">
+				<div>
+					<div class="uppercase section-title text-white text-center weight-700 size-12">Featured Fighter</div>
+					<h3 class="side-featuredfighter-title text-white uppercase size-17"><?=$fighter->post_title?></h3>
+				</div>
+				<figure>
+					<img class="img-responsive <?=($sideview ? 'sideview' : 'thumbnail')?>" src="<?=$imagesrc?>" alt="" />
+				</figure>
+			</div>
+		<?php endforeach; ?>
+	<?php endif; ?>
