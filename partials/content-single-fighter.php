11d20803216fe7ba864424d20ff9ce21eaaf9da8
Ralph John Galindo <ralphjesy@gmail.com>
2016-10-14 14:23:20 +0800

    Preparing Single Fighter Page

---
+++ partials/content-single-fighter.php
@@ -0,0 +1,29 @@
+<section id="main-featured-fighter">
+    <div class="ff-bg-red"></div>
+    <div class="ff-bg-gray">
+        <div class="container padding-bottom-0">
+            <div class="figher-item fighter-solo">
+                <div class="col-sm-4">
+                </div>
+                <div class="col-sm-8 fighter-details">
+                        <p class="fighter-profile-motto text-white size-16">Fabulas definitiones ei pri per recteque hendrerit scriptorem in errem scribentur mel fastidii propriae philosophia cu mea.</p>
+                    <div class="fighter-name">
+                        <label class="name-line-1 uppercase">Erick</label>
+                        <label class="name-line-2 uppercase">Silva</label>
+                    </div>
+                    <figure style="background-image: url('http://beta.cagegladiators.com/wp-content/uploads/2016/10/Norifumi-Yamamoto_104843_RightFullBodyImage.png')">
+                    </figure>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+
+<section id="content">
+
+    <div class="container">
+        <div class="row">
+
+        </div>
+    </div>
+</section>
--- single.php
+++ single.php
@@ -1,9 +1,5 @@
 <?php
-	get_header();
-?>
-<section>
-	single content here
-</section>
-<?php
-	get_footer();
-?>
+global $post;
+get_header();
+get_template_part("partials/content", locate_template('partials/content-single-'.get_post_type().'.php')!='' ? 'single-'.get_post_type() : 'single' );
+get_footer();
