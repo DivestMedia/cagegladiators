d9934f7ca4b9b8ff57b92e2aa8bfb6501d2f3315
ljopleda <ljopleda@gmail.com>
2016-10-24 09:17:12 +0800

    News template addedd

--- partials/content-fighter-media.php
+++ partials/content-fighter-media.php
@@ -4,15 +4,15 @@
 	<section id="section-media-gallery">
 		<div class="container">
 			<div class="row">
-				<!-- <div class="col-md-3 container-fighter-info">
-					<h1><?=$fighter->post_title?></h1> -->
-					<!-- <img class="img-responsive noradius" src="<?=wp_get_attachment_image_src(get_post_meta( $fighter->ID, '_uf_image_left', true ),'full')[0]?>" alt="" /> -->
-					<!-- <p><?=$fighter->post_content?></p> -->
-				<!-- </div> -->
+				<!-- <div class="col-md-2 container-fighter-info">
+					<h1><?=$fighter->post_title?></h1>
+					<img class="img-responsive noradius" src="<?=wp_get_attachment_image_src(get_post_meta( $fighter->ID, '_uf_image_left', true ),'full')[0]?>" alt="" />
+					<p><?=$fighter->post_content?></p>
+				</div>  -->
 				<div class="col-md-9">
 					<h3 class="header-red"><?=$fighter->post_title?> Media Gallery</h3>
                         <?php if(!empty($media_gallery)){?>
-                        <div class="masonry-gallery columns-3 margin-bottom-20 clearfix lightbox" data-img-big="1" data-plugin-options='{"delegate": "a", "gallery": {"enabled": true}}'>
+                        <div class="masonry-gallery columns-4 margin-bottom-20 clearfix lightbox" data-img-big="1" data-plugin-options='{"delegate": "a", "gallery": {"enabled": true}}'>
                             <?php
                                 foreach ($media_gallery as $key => $img) {
                             ?>
@@ -25,7 +25,7 @@
                         </div>
                         <?php }?>
 				</div>
-				<div class="col-md-3">
+				<div class="col-md-2">
 					<?php dynamic_sidebar('ads-home-left')?>
 				</div>
 			</div>
