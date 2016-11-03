abc3b2c11d6913c7975a5feaaaf692480f9c8134
ljopleda <ljopleda@gmail.com>
2016-11-03 16:42:36 +0800

    Featured fighter design update

--- partials/content-archive-fighter.php
+++ partials/content-archive-fighter.php
@@ -1,5 +1,5 @@
 <?php
-	
+	$hide_old = 1;
 	$featured_fighter = get_posts([
 						'posts_per_page'   => 6,
 						'orderby'          => 'rand',
@@ -15,68 +15,68 @@
 					]);
 
 	$current_page_info = get_queried_object();
-	if(!empty($current_page_info->slug)){
-		$fighters = get_posts([
-						'posts_per_page'   => 12,
-						'paged' 			=> get_query_var('paged') ?: 1,
-						'orderby'          => 'title',
-						'order'            => 'ASC',
-						'post_type'        => 'fighter',
-						'post_status'      => 'publish',
-						'tax_query' => array(
-				            array(
-				                'taxonomy' => 'fighters',
-				                'field' => 'slug',
-				                'terms' => $current_page_info->slug,
-				            )
-				        ),
-				        'meta_query' => array(
-				        	'relation' => 'OR',
-							array(
-								'key' => '_is_featured',
-								'value' => '0',
-							),
-							array(
-								'key' => '_is_featured',
-								'compare' => 'NOT EXISTS'
-							),
-						),
-						'suppress_filters' => true
-					]);
-	}else{
-		$fighters = get_posts([
-						'posts_per_page'   => 12,
-						'paged' 			=> get_query_var('paged') ?: 1,
-						'orderby'          => 'title',
-						'order'            => 'ASC',
-						'post_type'        => 'fighter',
-						'post_status'      => 'publish',
-						'tax_query' => array(
-				            array(
-				                'taxonomy' => 'fighters',
-				                'field' => 'slug',
-				                'operator' => 'EXISTS'
-				            )
-				        ),
-				        'meta_query' => array(
-				        	'relation' => 'OR',
-							array(
-								'key' => '_is_featured',
-								'value' => '0',
-							),
-							array(
-								'key' => '_is_featured',
-								'compare' => 'NOT EXISTS'
-							),
-						),
-						'suppress_filters' => true
-					]);
-	}
+	// if(!empty($current_page_info->slug)){
+	// 	$fighters = get_posts([
+	// 					'posts_per_page'   => 12,
+	// 					'paged' 			=> get_query_var('paged') ?: 1,
+	// 					'orderby'          => 'title',
+	// 					'order'            => 'ASC',
+	// 					'post_type'        => 'fighter',
+	// 					'post_status'      => 'publish',
+	// 					'tax_query' => array(
+	// 			            array(
+	// 			                'taxonomy' => 'fighters',
+	// 			                'field' => 'slug',
+	// 			                'terms' => $current_page_info->slug,
+	// 			            )
+	// 			        ),
+	// 			        'meta_query' => array(
+	// 			        	'relation' => 'OR',
+	// 						array(
+	// 							'key' => '_is_featured',
+	// 							'value' => '0',
+	// 						),
+	// 						array(
+	// 							'key' => '_is_featured',
+	// 							'compare' => 'NOT EXISTS'
+	// 						),
+	// 					),
+	// 					'suppress_filters' => true
+	// 				]);
+	// }else{
+	// 	$fighters = get_posts([
+	// 					'posts_per_page'   => 12,
+	// 					'paged' 			=> get_query_var('paged') ?: 1,
+	// 					'orderby'          => 'title',
+	// 					'order'            => 'ASC',
+	// 					'post_type'        => 'fighter',
+	// 					'post_status'      => 'publish',
+	// 					'tax_query' => array(
+	// 			            array(
+	// 			                'taxonomy' => 'fighters',
+	// 			                'field' => 'slug',
+	// 			                'operator' => 'EXISTS'
+	// 			            )
+	// 			        ),
+	// 			        'meta_query' => array(
+	// 			        	'relation' => 'OR',
+	// 						array(
+	// 							'key' => '_is_featured',
+	// 							'value' => '0',
+	// 						),
+	// 						array(
+	// 							'key' => '_is_featured',
+	// 							'compare' => 'NOT EXISTS'
+	// 						),
+	// 					),
+	// 					'suppress_filters' => true
+	// 				]);
+	// }
 
-	$fighter_categories = get_terms( array(
-	    'taxonomy' => 'fighters',
-	    'hide_empty' => false,
-	) );
+	// $fighter_categories = get_terms( array(
+	//     'taxonomy' => 'fighters',
+	//     'hide_empty' => false,
+	// ) );
 
 	if(empty($fighters)){
 		// die();
@@ -84,7 +84,7 @@
 	if(empty($current_page_info->slug)&&get_query_var('paged')==0){
 ?>
 
-<section class="dark section-fighters">
+<section class="dark section-fighters mod-section-fighters">
 	<div class="featured-figter-container">
 		<div class="container">
 			<header class="text-center margin-bottom-30">
@@ -96,6 +96,8 @@
 	    			$_r_win = get_post_meta( $fighter->ID, '_uf_win', true );
 	    			$_r_loss = get_post_meta( $fighter->ID, '_uf_loss', true );
 	    			$_r_draw = get_post_meta( $fighter->ID, '_uf_draw', true );
+	    			$_image_right = get_post_meta( $fighter->ID, '_uf_image_right', true );
+
 	    			$record = $_r_win.'-'.$_r_loss.'-'.$_r_draw;
 	    			$_r_fighter_terms = get_the_terms($fighter->ID,'fighters');
 	    			$f_weight_class = '';
@@ -105,7 +107,33 @@
 						$f_weight_class_slug = $_r_fighter_terms[0]->slug;
 					}
 			?>	
-					<div class="col-sm-6 col-md-4 container-fighters">
+					<div class="col-md-4 margin-bottom-30 mod-container-fighters">
+						<div class="box-flip box-icon box-icon-center box-icon-round box-icon-large text-center">
+							<div class="front">
+								<div class="box1 noradius padding-0">
+									<div class="thumbnail">
+										<img class="img-responsive" src="<?=wp_get_attachment_image_src($_image_right,'full')[0]?>" alt="" style="height: 400px; width: auto;" />
+										<div class="caption">
+											<h3 class="margin-bottom-10"><strong><?=$fighter->post_title?></strong></h3 class="margin-bottom-10"></a>
+											<small class="block lbl-weightclass"><strong><?=$f_weight_class?></strong></small>
+											<small class="record-cont"><?=$record?></small>
+										</div>
+									</div>								</div>
+							</div>
+
+							<div class="back">
+								<div class="box2">
+									<a href="<?=get_permalink($fighter->ID)?>"><h4 class="margin-bottom-10"><strong><?=$fighter->post_title?></strong></h4></a>
+									<small class="block lbl-weightclass"><strong><?=$f_weight_class?></strong></small>
+									<small class="record-cont"><?=$record?></small>
+									<hr />
+									<p><?=trim_text($fighter->post_content,500)?></p>
+									<a href="<?=get_permalink($fighter->ID)?>" class="btn btn-translucid btn-lg btn-block btn-red noradius btn-sm">View Profile</a>
+								</div>
+							</div>
+						</div>
+					</div>
+					<!-- <div class="col-sm-6 col-md-4 container-fighters">
 						<a href="<?=get_permalink($fighter->ID)?>">
 							<div class="thumbnail nopadding-bottom margin-bottom-0 noradius">
 								<img class="img-responsive noradius" src="<?=wp_get_attachment_image_src(get_post_thumbnail_id($fighter->ID),'full')[0]?>" alt="" />
@@ -116,7 +144,7 @@
 							<a href="<?=site_url('/fighters/'.$f_weight_class_slug)?>"><small class="block lbl-weightclass"><strong><?=$f_weight_class?></strong></small></a>
 							<small class="record-cont"><?=$record?></small>
 						</div>
-					</div>
+					</div> -->
 			<?php
 				}
 			?>
@@ -124,7 +152,9 @@
 		</div>
 	</div>
 </section>
-<?php }?>
+<?php }
+if(!$hide_old){
+?>
 
 <section id="section-fighters" class="section-fighters">
 	
@@ -204,3 +234,4 @@
 		</div>
 	</div>
 </section>
+<?php }?>
